55ab71e3673f3a1b0f110b08184a02e42dd39642
Neost2 <Neost2@hotmail.com>
2024-09-15 16:58:14 -0500

    new branch put into a switch

--- MODQuarters.c
+++ MODQuarters.c
@@ -48,52 +48,67 @@ int Bet(int bet1, int quarters)
 
 
 int validBET(int bet, int num1, int quarters1)
+{
+    int num2;
+    if (bet == 0)
     {
-       if (bet == 0)
-       {
         return bet, num, quarters;
-       }else if (num >= 95)
-        {
-            printf("Reel stopped on the number: %d \n", num);
-            quarters = (quarters - bet);
-            printf("Jackpot! You triple your money!\n\n");
-            quarters = (quarters + (bet * 3));
-            
-            return bet, num, quarters;
-        }
-        else if (num < 50)
-        {
-
-            printf("Reel stopped on the number: %d \n", num);
-            printf("You lose!\n\n");
-            quarters = (quarters - bet);
-          
-            return bet, num, quarters;
-        }
-        else if ((num >= 51) && (num <= 74))
-        {
-
-            printf("Reel stopped on the number: %d \n", num);
-            quarters = (quarters - bet);
-            printf("You get your money back! \n\n");
-            quarters = (quarters + bet);
-         
-           return bet, num, quarters;
-
-        }
-        else if ((num >= 75) && (num <= 94))
-        {
-
-            printf("Reel stopped on the number: %d \n", num);
-            quarters = (quarters - bet);
-            printf("You doubled your money! \n\n");
-            quarters = (quarters + (bet * 2));
-            
-           return bet, num, quarters;
-        }
-
-
+    }else if (num >= 95)
+    {
+        num2 = 1;
     }
+    else if (num < 50)
+    {
+        num2 = 2;
+    }else if ((num >= 51) && (num <= 74))
+    {
+        num2 = 3;
+    }
+    else if ((num >= 75) && (num <= 94))
+    {
+        num2 = 4;
+    }
+    switch (num2)
+    {
+    case 1:
+        printf("Reel stopped on the number: %d \n", num);
+        quarters = (quarters - bet);
+        printf("Jackpot! You triple your money!\n\n");
+        quarters = (quarters + (bet * 3));
+
+        return bet, num, quarters;
+        break;
+    case 2:
+        printf("Reel stopped on the number: %d \n", num);
+        printf("You lose!\n\n");
+        quarters = (quarters - bet);
+
+        return bet, num, quarters;
+        break;
+    case 3:
+        printf("Reel stopped on the number: %d \n", num);
+        quarters = (quarters - bet);
+        printf("You get your money back! \n\n");
+        quarters = (quarters + bet);
+
+        return bet, num, quarters;
+        break;
+    case 4:
+        printf("Reel stopped on the number: %d \n", num);
+        quarters = (quarters - bet);
+        printf("You doubled your money! \n\n");
+        quarters = (quarters + (bet * 2));
+
+        return bet, num, quarters;
+        break;
+    default:
+        printf("Invalid number something wrong");
+        return bet, num, quaters;
+        }
+       
+       
+
+}
 
 
 int main(void)
